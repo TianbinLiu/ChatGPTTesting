bb361f4a614adc35d5a0450c8fa97a30dd3715c5
TianbinLiu <l196521542@gmail.com>
2023-06-01 23:55:10 -0700

    2

--- script.js
+++ script.js
@@ -1,46 +1,33 @@
 //tutorial: https://www.youtube.com/watch?v=4qNwoAAfnk4
 //openai documentation(python): https://platform.openai.com/docs/guides/chat
 
-import { config } from './node_modules/dotenv/config.js';
+import { config } from "dotenv"
 config()
 
 import { Configuration, OpenAIApi } from "openai"
+import readline from "readline"
 
-const openai = new OpenAIApi(new Configuration({
-  apiKey: process.env.API_KEY
-}))
-
-
-const dialogElement = document.getElementById("dialog");
-const userInputElement = document.getElementById("user-input");
-const submitButton = document.getElementById("submit-btn");
-
-let userInputArray = [];
-
-function appendUserInput(input) {
-  const userInputItem = document.createElement("div");
-  userInputItem.innerText = input;
-  dialogElement.appendChild(userInputItem);
-}
-
-function handleUserInput() {
-  const userInput = userInputElement.value;
-  const res = openai.createChatCompletion({
-    model:"gpt-3.5-turbo",
-    messages: [{role:"user", content: userInput}],
+const openAi = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.API_KEY,
   })
-  console.log(res.data.choices[0].message.content) //print out result(output)
-  userInputArray.push(userInput);
-  appendUserInput(userInput);
-  userInputElement.value = "";
-}
+)
 
-submitButton.addEventListener("click", handleUserInput);
-userInputElement.addEventListener("keydown", function (event) {
-  if (event.key === "Enter") {
-    handleUserInput();
-  }
-});
+const userInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+userInterface.prompt()
+userInterface.on("line", async input => {
+  const response = await openAi.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: input }],
+  })
+  console.log(response.data.choices[0].message.content)
+  userInterface.prompt()
+})
+
 
 
 
