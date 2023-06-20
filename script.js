d10ed648b957147510a08fc6a271d1fa0cec2119
Devin Han <39814387+laladehulu@users.noreply.github.com>
2023-06-19 21:19:42 -0700

    Allow CORS

--- script.js
+++ script.js
@@ -11,34 +11,34 @@ app.use(express.json());
 const allowedOrigins = ["http://127.0.0.1:5500/", "http://127.0.0.1:5500/public/index.html"];
 
 const openAi = new OpenAIApi(
-new Configuration({
-apiKey: process.env.API_KEY,
+  new Configuration({
+    apiKey: process.env.API_KEY,
 
-}))
+  }))
 
 app.use(cors({
-origin: function(origin, callback) {
-console.log("COR request from ", origin);
-return callback(null, true);
-}
+  origin: function(origin, callback) {
+      console.log("COR request from ", origin);
+      return callback(null, true);
+  }
 }))
 
 app.get("/api/chat",(req, res) => {
-res.send("Chat API");
+  res.send("Chat API");
 });
 
 app.post("/api/chat", async (req, res) => {
-const { input } = req.body;
+  const { input } = req.body;
 
-const response = await openAi.createChatCompletion({
-model: "gpt-3.5-turbo",
-messages: [{ role: "user", content: input }],
-});
+  const response = await openAi.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: input }],
+  });
 
-const output = response.data.choices[0].message.content;
-res.json({ output });
+  const output = response.data.choices[0].message.content;
+  res.json({ output });
 });
 
 app.listen(3000, () => {
-console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
