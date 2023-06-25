c708e9827747ae8cbe533ef842f1d7ea920cc12a
Devin Han <39814387+laladehulu@users.noreply.github.com>
2023-06-24 21:55:59 -0700

    FULLCHAIN

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 const allowedOrigins = ["http://127.0.0.1:5500/", "http://127.0.0.1:5500/public/index.html"];
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/chatgpttesting.lol/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/chatgpttesting.lol/cert.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/chatgpttesting.lol/fullchain.pem', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 const openAi = new OpenAIApi(
   new Configuration({
