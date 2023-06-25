908e672820f0dc6a2796329a12575944bdd0821a
Devin Han <39814387+laladehulu@users.noreply.github.com>
2023-06-24 22:12:40 -0700

    Full version

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ const app = express();
 
 app.use(express.json());
 const allowedOrigins = ["http://127.0.0.1:5500/", "http://127.0.0.1:5500/public/index.html"];
-/*
+
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/chatgpttesting.lol/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/chatgpttesting.lol/fullchain.pem', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
-*/
+
 const openAi = new OpenAIApi(
   new Configuration({
     apiKey: process.env.API_KEY,
@@ -46,9 +46,7 @@ app.post("/api/chat", async (req, res) => {
   res.json({ output });
 });
 var httpServer = http.createServer(app);
-//var httpsServer = https.createServer(credentials, app);
-app.listen(3000,()=>{
-console.log("this should work ");
-})
-  
-//httpsServer.listen(443);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80);
+httpsServer.listen(443);
