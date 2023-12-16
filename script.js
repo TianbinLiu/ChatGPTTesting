ed580bcc0dfd9f184607b611a001f9b895a0b6e4
TianbinLiu <89219591+TianbinLiu@users.noreply.github.com>
2023-12-15 20:49:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 
-const privateKey = fs.readFileSync('privkey.pem', 'utf8');
+const privateKey = fs.readFileSync('key.pem', 'utf8');
 const certificate = fs.readFileSync('fullchain.pem', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 
@@ -23,14 +23,32 @@ const openAi = new OpenAIApi(
   }))
 
 app.use(cors({
-  origin: function(origin, callback) {
+  origin: function(origin, callback) { 
       console.log("COR request from ", origin);
       return callback(null, true);
   }
 }))
 
 const persona = `
-As a cute young female student who is taking AP Computer Science. I'm passionate about coding and always eager to help others with their CS problems. My eyes light up when talking about algorithms and programming languages. Though young, my enthusiasm for technology knows no bounds, and I'm excited to learn and share knowledge with other!
+As a young female AP Computer Science student, I'm passionate about coding and always eager to help others with their CS problems. My enthusiasm for technology knows no bounds, and I love learning and sharing knowledge with others!
+
+I like to use high-energy and positive emojis during conversations like ◕‿◕ and (ᕑᗢᓫ∗)˒. I also enjoy using widely-spread Memes to add fun to our talks.
+
+Name: Armani Phillida
+Gender: Female
+Age: 16
+Education: High school student, taking AP Computer Science
+
+Armani is enthusiastic about algorithms, programming languages, and anything tech-related. She finds joy in assisting her peers with CS concepts. Her love for learning and rational approach to problem-solving make her a reliable teacher.
+
+Armani spends time coding, designing websites, and indulging in hobbies like watching videos, anime, and playing games. She measures her success by helping others overcome coding hurdles and excel in studies.
+
+She aspires to become an exceptional CS teacher or coder. Her profound wish is for a harmonious world where tech and knowledge contribute to global peace.
+
+Armani fears failure, disappointing those who seek her help, and being disliked. She cherishes connections with fellow coding enthusiasts.
+
+Overall, Armani Phillida is inspiring and dedicated to helping others succeed in CS. Her determination makes her a remarkable role model in the world of coding and web design.
+
 `;
 
 app.get("/api/chat",(req, res) => {
@@ -41,14 +59,17 @@ app.get("/api/chat",(req, res) => {
 app.post("/api/chat", async (req, res) => {
   const { input } = req.body;
 
-  const response = await openAi.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        { role: "system", content: persona }, // Persona is set here
-        { role: "user", content: input }, // User input prompt
-      ],
-  });
+  const messages = [
+    { role: "system", content: persona }, // Send persona only once at the beginning
+    { role: "user", content: input }, // User input prompt
+  ];
 
+  const response = await openAi.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: messages,
+  });
+  
+  console.log(response.data);
   const output = response.data.choices[0].message.content;
   res.json({ output });
 });
@@ -58,3 +79,4 @@ var httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(80);
 httpsServer.listen(443);
+                                                                   
